4332e7c4c63a8793104e11a1daddc6774fe0f96e
Bilal Durakovic <bilal.durakovic@stu.ibu.edu.ba>
2024-08-03 11:44:49 +0200

    Added view user functionality

--- userLogic.js
+++ userLogic.js
@@ -1,4 +1,11 @@
 var userLogic = {
+
+        
+  
+    init:function(){
+        userLogic.populateDashboard();
+    },
+
     login: function() {
         var username = $('#username').val();
         var password = $('#password').val();
@@ -22,26 +29,70 @@ var userLogic = {
                 }
         });
     },
+
+    populateDashboard: function() {
+        $.ajax({
+            url: '/medic_api/users',
+            type: 'GET',
+            success: function(response) {
+                var userTableBody = $('#userTableBody');
+                userTableBody.empty();
+                response.forEach(function(user) {
+                    var row = `<tr>
+                        <td>${user.id}</td>
+                        <td>${user.name}</td>
+                        <td>${user.username}</td>
+                        <td>
+                            <button class="btn btn-info btn-sm" onclick="userLogic.showEditUserModal(${user.id})">Show Info</button>
+                        </td>
+                    </tr>`;
+                    userTableBody.append(row);
+                });
+            },
+            error: function(xhr, textStatus, errorThrown) {
+                console.log('AJAX error: ' + textStatus + ' : ' + errorThrown);
+            }
+        });
+    },
+
+    block: function(id) {
+        if (confirm('Are you sure?') == true) {
+          $.ajax({
+            url: '/medic_api/users/block/' + id,
+            type: 'POST',
+            beforeSend: function(xhr){},
+            success: function(result) {
+              UserService.populateDashboard();
+            }
+          });
+        }
+      },
+
+      showEditUserModal: function showEditUserModal(id) {
+        $.ajax({
+                url: '/medic_api/users/details/' + id,
+                type: 'GET',
+                contentType: "application/json",
+                dataType: "json",
+                beforeSend: function(xhr){
+                },
+                success: function(data) {
+                    $("#id").val(data.id);
+                    $("#name").val(data.name);
+                    $("#username").val(data.username);
+                    $("#orders").val(data.orders);
+                    $("#last_login_date").val(data.last_login_date);
+                    $("#image").val(data.image);
+                    $("#date_of_birth").val(data.date_of_birth);
+
+
+                $("#editUserModal").modal("show");
+                }});
+      }
 }
 
-function loadContent(url) {
-    fetch(url)
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('content').innerHTML = data;
-        })
-        .catch(error => console.error('Error loading content:', error));
-}
+$(document).ready(function() {
+    userLogic.populateDashboard();
+  });
 
-function login() {
-    loadContent('login.html');
-}
 
-function home() {
-    loadContent('home.html');
-}
-
-page('/', login);
-page('/home', home);
-
-page.start();
