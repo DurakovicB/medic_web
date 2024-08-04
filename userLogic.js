96611ffa74f49b8a105ccba908f36c0e77bec6a3
Bilal Durakovic <bilal.durakovic@stu.ibu.edu.ba>
2024-08-04 16:50:24 +0200

    modified functionalities; added login loader

--- userLogic.js
+++ userLogic.js
@@ -10,12 +10,13 @@ var userLogic = {
               userLogic.add(user);
             }
           });
+
         userLogic.populateDashboard();
     },
 
-    login: function() {
-        var username = $('#username').val();
-        var password = $('#password').val();
+    login: function(user) {
+        var username = $("#username").val();
+        var password = $("#password").val();
 
         $.ajax({
             url: 'http://localhost/medic_api/login',
@@ -24,16 +25,14 @@ var userLogic = {
                 username: username,
                 password: md5(password)
             },
-            success: function(response) {
-                if (response.status == 'success') {
-                    window.location.href='/medic_web/home.html'
-
-                } else {
-                    alert(response.message);
-                }
+            success: async function(response) {
+                localStorage.setItem("token", response.token);
+                await spinLoaderForTwoSeconds();
+                window.location.href='/medic_web/home.html';
             },
-            error: function(xhr) {
-                }
+            error: function (xhr, tst, err) {
+                alert(JSON.parse(xhr.responseText)["message"]);
+            },
         });
     },
 
@@ -45,13 +44,11 @@ var userLogic = {
                 var userTableBody = $('#userTableBody');
                 userTableBody.empty();
                 response.forEach(function(user) {
-                    var row = `<tr>
+                    var row = `<tr class="clickable-row" onclick="userLogic.showEditUserModal(${user.id})">
                         <td>${user.id}</td>
                         <td>${user.name}</td>
                         <td>${user.username}</td>
-                        <td>
-                            <button class="btn btn-info btn-sm" onclick="userLogic.showEditUserModal(${user.id})">Show Info</button>
-                        </td>
+                        <td>${user.last_login_date}</td>
                     </tr>`;
                     userTableBody.append(row);
                 });
@@ -90,6 +87,8 @@ var userLogic = {
                     $("#orders").val(data.orders);
                     $("#last_login_date").val(data.last_login_date);
                     $("#image").val(data.image);
+                    $("#imageDiv").attr("src",data.image);
+
                     $("#status").val(data.status);
                     $("#date_of_birth").val(data.date_of_birth);
 
@@ -104,6 +103,7 @@ var userLogic = {
         user.image=$("#image").val();
         user.status=$("#status").val();
         user.date_of_birth=$("#date_of_birth").val();
+        user.last_login_date=$("#last_login_date").val();
 
         $.ajax({
             url: '/medic_api/users/update/' + $('#id').val(),
@@ -134,7 +134,10 @@ var userLogic = {
             $("#addUserModal").modal("hide");
             $('#addUserForm').trigger("reset");
             userLogic.populateDashboard();
-          }
+          },
+          error: function (xhr, tst, err) {
+            alert(JSON.parse(xhr.responseText)["message"]);
+        }
         });
       },
 
